1b3fafe879147efcf98723ede22c887343162db2
Ji Eun Park <jep051@ucsd.edu>
2018-02-22 09:42:51 -0800

    Pre- A/B test

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -9,5 +9,11 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	// your code here
+	$(".likeBtn").click(function(){
+ 		ga("send", "event", 'like', 'click');
+ 	});
+ 	$(".btn.btn-default.likeBtn").click(function(){
+		ga("send", "event", 'like', 'click');
+ });
+	
 }
